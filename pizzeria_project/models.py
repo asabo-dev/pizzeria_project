7c0e2440669ab4cbf26f64a9e844c34aa05838a1
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-09-17 18:34:01 +0800

    define models

--- pizzeria_project/models.py
+++ pizzeria_project/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    """A pizza the user can make."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+class Topping(models.Model):
+    """Toppings for the pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
