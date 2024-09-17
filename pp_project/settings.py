ca1bbce1a07c69bb6e32d385e9adbdd4c9f6f52c
Efiom Ndaeyo <rylshadow@yahoo.com>
2024-09-17 18:37:30 +0800

    activate models

--- pp_project/settings.py
+++ pp_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'pizzeria',
+    # The Django admin.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
